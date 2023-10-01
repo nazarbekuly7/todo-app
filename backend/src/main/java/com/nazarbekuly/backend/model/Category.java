7881e20c61a8b2a7dcff847c2a2be726eb51a3c5
nazarbekuly7 <tifixgame@gmail.com>
2023-10-01 15:06:57 +0600

    create Category model

---
+++ backend/src/main/java/com/nazarbekuly/backend/model/Category.java
@@ -0,0 +1,32 @@
+package com.nazarbekuly.backend.model;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    @JsonIgnore
+    private User user;
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+    private List<Todo> todoList;
+}
